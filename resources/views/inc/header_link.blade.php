bbc29c82ecb801fb4e74c4e25985a831d8e4b895
smaheshacharya <mahesh01acharya@gmail.com>
2020-07-05 16:10:09 +0545

    setup overalll front end

---
+++ resources/views/inc/header_link.blade.php
@@ -0,0 +1,29 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+<!-- StyleSheet -->
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="{{asset('css/magnific-popup.min.css')}}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+<!-- Fancybox -->
+<link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}}">
+<!-- Themify Icons -->
+<link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+<!-- Nice Select CSS -->
+<link rel="stylesheet" href="{{asset('css/niceselect.css')}}">
+<!-- Animate CSS -->
+<link rel="stylesheet" href="{{asset('css/animate.css')}}">
+<!-- Flex Slider CSS -->
+<link rel="stylesheet" href="{{asset('css/flex-slider.min.css')}}">
+<!-- Owl Carousel -->
+<link rel="stylesheet" href="{{asset('css/owl-carousel.css')}}">
+<!-- Slicknav -->
+<link rel="stylesheet" href="{{asset('css/slicknav.min.css')}}">
+
+<!-- Eshop StyleSheet -->
+<link rel="stylesheet" href="{{asset('css/reset.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/responsive.css')}}">
---
+++ resources/views/layouts/app1.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<!-- Meta Tag -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name='copyright' content=''>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Title Tag  -->
+    <title>Eshop - eCommerce HTML5 Template.</title>
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<!-- Web Font -->
+{{-- HEADER link PART     --}}
+@include('inc.header_link')
+{{-- HEADER NAV PART     --}}
+    @include('inc.header')
+
+{{-- BODY PART --}}
+@yield('body')
+
+{{-- FOOTER PART --}}
+    @include('inc.footer')
+{{-- FOOTER LINK PART --}}
+    @include('inc.footer')
+
+    </body>
+    </html>
